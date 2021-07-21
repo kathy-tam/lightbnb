53aee4441daa900b18d0b74c7b5d9bc684910c78
Kathy <kathy.tam@mail.mcgill.ca>
2021-07-21 16:42:54 +0000

    Correct typo

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -1,6 +1,6 @@
 INSERT INTO users (name, email, password)
 VALUES ('Eva Stanley', 'evastanley@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
-('Etta WEst', 'ettawest@yahoo.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
+('Etta West', 'ettawest@yahoo.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
 ('Dominic Parks', 'dominicparks@hotmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
 
 INSERT INTO properties(owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active)
